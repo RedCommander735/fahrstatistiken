525b625d6d55bdb686ba55336bfee14fdefda2b0
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-06-08 18:21:07 +0200

    Added Screen for user inputs, currently broken

--- app/src/main/java/de/redcommander/spritstatistik/MainActivity.kt
+++ app/src/main/java/de/redcommander/spritstatistik/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.redcommander.spritstatistik
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,12 +11,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -24,6 +30,7 @@ import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults.colors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,9 +40,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.modifierElementOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -73,55 +85,55 @@ class MainActivity : ComponentActivity() {
                     verticalAlignment = Alignment.Bottom
                 ) {
                     if (!addValueScreenActive) {
-                    Box(
-                        modifier = Modifier
-                            .size(70.dp)
-                            .fillMaxSize()
-                            .clip(RoundedCornerShape(25.dp))
-                            .background(
-                                if (isSystemInDarkTheme()) {
-                                    Color(0xFF3C3C3C)
-                                } else {
-                                    Color(0xFFC3C3C3)
-                                }
-                            )
-                    ) {
-                        Row(
+                        Box(
                             modifier = Modifier
+                                .size(70.dp)
                                 .fillMaxSize()
-                                .clickable {
-                                    addValueScreenActive = true
-                                    navController.navigate(Screen.AddValue.route) {
-                                        // Pop up to the start destination of the graph to
-                                        // avoid building up a large stack of destinations
-                                        // on the back stack as users select items
-                                        popUpTo(navController.graph.findStartDestination().id) {
-                                            saveState = true
-                                        }
-                                        // Avoid multiple copies of the same destination when
-                                        // reselecting the same item
-                                        launchSingleTop = true
-                                        // Restore state when reselecting a previously selected item
-                                        restoreState = true
+                                .clip(RoundedCornerShape(25.dp))
+                                .background(
+                                    if (isSystemInDarkTheme()) {
+                                        Color(0xFF3C3C3C)
+                                    } else {
+                                        Color(0xFFC3C3C3)
                                     }
-                                },
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
+                                )
                         ) {
-                            Icon(
-                                painter = painterResource(Screen.AddValue.drawableId),
-                                contentDescription = null,
+                            Row(
                                 modifier = Modifier
-                                    .size(28.dp),
-                                tint = if (isSystemInDarkTheme()) {
-                                    Color.White
-                                } else {
-                                    Color.Black
-                                }
-                            )
+                                    .fillMaxSize()
+                                    .clickable {
+                                        addValueScreenActive = true
+                                        navController.navigate(Screen.AddValue.route) {
+                                            // Pop up to the start destination of the graph to
+                                            // avoid building up a large stack of destinations
+                                            // on the back stack as users select items
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            // Avoid multiple copies of the same destination when
+                                            // reselecting the same item
+                                            launchSingleTop = true
+                                            // Restore state when reselecting a previously selected item
+                                            restoreState = true
+                                        }
+                                    },
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Icon(
+                                    painter = painterResource(Screen.AddValue.drawableId),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(28.dp),
+                                    tint = if (isSystemInDarkTheme()) {
+                                        Color.White
+                                    } else {
+                                        Color.Black
+                                    }
+                                )
+                            }
                         }
                     }
-                }
                 }
             }
         }
@@ -171,17 +183,71 @@ fun DataListScreen() {
 fun AddValueScreen() {
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
     ) {
-        Text(
-            text = "AddValue Screen",
-            color = if (isSystemInDarkTheme()) {
-                Color.White
-            } else {
-                Color.Black
-            }
+        AddValueRow(R.drawable.gas_station, "Tankfüllung in Liter") {}
+        AddValueRow(R.drawable.road, "Kilometer gefahren") {}
+        AddValueRow(R.drawable.euro_symbol, "Spritpreis") {}
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = Color.White
+            )
+    ) {
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddValueRow(
+    icon: Int,
+    text: String = "placeholder"
+    updateValue: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .height(115.dp)
+            .fillMaxWidth()
+            .padding(top = 55.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        val iconSize = 48.dp
+        Box(
+            modifier = Modifier
+                .padding(end = 20.dp)
+        ) {
+            Icon(
+                painter = painterResource(icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(iconSize),
+                tint = if (isSystemInDarkTheme()) {
+                    Color.White
+                } else {
+                    Color.Black
+                }
+            )
+        }
+
+        TextField(
+            value = "",
+            label = {
+                Text(text = text)
+            }, onValueChange = {
+                updateValue(it)
+            },
+            modifier = Modifier.fillMaxWidth(0.7f),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
         )
     }
 }
