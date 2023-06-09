4a6c2b205ef54499a942917eb9892a599c52f425
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-06-09 22:04:32 +0200

    Finalized UI for adding an entry to the DB

--- app/src/main/java/de/redcommander/spritstatistik/MainActivity.kt
+++ app/src/main/java/de/redcommander/spritstatistik/MainActivity.kt
@@ -1,6 +1,5 @@
 package de.redcommander.spritstatistik
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,19 +10,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
@@ -31,6 +31,8 @@ import androidx.compose.material3.NavigationBarItemDefaults.colors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,15 +41,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.node.modifierElementOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -56,6 +56,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import de.redcommander.spritstatistik.ui.theme.SpritStatistikTheme
+import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -180,75 +181,214 @@ fun DataListScreen() {
 }
 
 @Composable
-fun AddValueScreen() {
+@Preview
+fun AddValueScreen(
+
+) {
+    Timber.plant(Timber.DebugTree())
+
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        AddValueRow(R.drawable.gas_station, "Tankfüllung in Liter") {}
-        AddValueRow(R.drawable.road, "Kilometer gefahren") {}
-        AddValueRow(R.drawable.euro_symbol, "Spritpreis") {}
+
+        var gas by remember {
+            mutableStateOf(0)
+        }
+        var distance by remember {
+            mutableStateOf(0)
+        }
+        var price by remember {
+            mutableStateOf(0.0)
+        }
+
+        var checkValid by remember {
+            mutableStateOf(false)
+        }
+
+        var isInvalid = false
+
+        AddValueRow(R.drawable.gas_station, stringResource(id = R.string.addValueLiters), checkValidity = checkValid, isInvalid = {_isInvalid -> isInvalid = _isInvalid}) {
+            if (it.isNotEmpty()) {
+                gas = it.toInt()
+            }
+        }
+        AddValueRow(R.drawable.road, stringResource(id = R.string.addValueKilometers), checkValidity = checkValid, isInvalid = {_isInvalid -> isInvalid = _isInvalid}) {
+            if (it.isNotEmpty()) {
+                distance = it.toInt()
+            }
+        }
+        AddValueRow(R.drawable.euro_symbol, stringResource(id = R.string.addValuePrice), checkValidity = checkValid, isInt = false, isInvalid = {_isInvalid ->
+            isInvalid = _isInvalid
+            checkValid = false
+        }) {
+            if (it.isNotEmpty()) {
+                price = it.toDouble()
+                Timber.d("Price: $price")
+            }
+        }
+
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 20.dp)
+        ) {
+            Button(
+                onClick = {checkValid = true},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (isSystemInDarkTheme()) {
+                        Color(0xFF3C3C3C)
+                    } else {
+                        Color(0xFFEBEBEB)
+                    }
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.addButton),
+                    color = if (isSystemInDarkTheme()) {
+                        Color.White
+                    } else {
+                        Color.Black
+                    }
+                )
+            }
+        }
     }
 }
 
-@Preview
-@Composable
-fun Preview() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                color = Color.White
-            )
-    ) {
-
-    }
-
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddValueRow(
     icon: Int,
-    text: String = "placeholder"
+    text: String = "placeholder",
+    isInt: Boolean = true,
+    checkValidity: Boolean = false,
+    isInvalid: (Boolean) -> Unit,
     updateValue: (String) -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .height(115.dp)
-            .fillMaxWidth()
-            .padding(top = 55.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        val iconSize = 48.dp
-        Box(
-            modifier = Modifier
-                .padding(end = 20.dp)
-        ) {
-            Icon(
-                painter = painterResource(icon),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(iconSize),
-                tint = if (isSystemInDarkTheme()) {
-                    Color.White
-                } else {
-                    Color.Black
-                }
-            )
-        }
+    Timber.plant(Timber.DebugTree())
 
-        TextField(
-            value = "",
-            label = {
-                Text(text = text)
-            }, onValueChange = {
-                updateValue(it)
-            },
-            modifier = Modifier.fillMaxWidth(0.7f),
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-        )
+
+
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+
+    var isError by remember {
+        mutableStateOf(false)
+    }
+
+    fun validate(text: String) {
+        val valid = text.isEmpty()
+        isError = valid
+        isInvalid(valid)
+    }
+
+    if (checkValidity) {
+        validate(textFieldState)
+    }
+
+    Box(
+        modifier = Modifier
+            .padding(top = 55.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .height(55.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            val iconSize = 48.dp
+            Box(
+                modifier = Modifier
+                    .padding(end = 20.dp)
+            ) {
+                Icon(
+                    painter = painterResource(icon),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(iconSize),
+                    tint = if (isSystemInDarkTheme()) {
+                        Color.White
+                    } else {
+                        Color.Black
+                    }
+                )
+            }
+
+            val textFieldColors: TextFieldColors = if (isSystemInDarkTheme()) {
+                TextFieldDefaults.textFieldColors(
+                    cursorColor = Color.White,
+                    containerColor = Color(0xFF3C3C3C),
+                    focusedIndicatorColor = Color.White,
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    errorIndicatorColor = MaterialTheme.colorScheme.error
+                )
+            } else {
+                TextFieldDefaults.textFieldColors(
+                    cursorColor = Color.Black,
+                    containerColor = Color(0xFFEBEBEB),
+                    focusedIndicatorColor = Color.Black,
+                    focusedLabelColor = Color.Black,
+                    unfocusedLabelColor = Color.Black,
+                    errorIndicatorColor = MaterialTheme.colorScheme.error
+                )
+            }
+
+            TextField(
+                value = textFieldState,
+                label = {
+                    Text(text = text)
+                },
+                onValueChange = {
+                    val invalidCharCount = Regex("([^0-9,.])").findAll(it).count()
+                    if (invalidCharCount == 0) {
+                        validate(it)
+                        if (isInt) {
+                            val value = it.replace(".", "").replace(",", "")
+                            textFieldState = value
+                            updateValue(value)
+                        } else {
+                            val commaCount = Regex("([,.])").findAll(it).count()
+                            if (it.length > 1 || commaCount == 0) {
+                                if (commaCount <= 1) {
+                                    textFieldState = it
+                                    updateValue(it.replace(",", "."))
+                                }
+                            }
+                        }
+                    }
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+                    .height(64.dp)
+                    .onFocusChanged {
+                        if (it.hasFocus) {
+                            validate(textFieldState)
+                        }
+                    },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                keyboardActions = KeyboardActions { validate(textFieldState) },
+                colors = textFieldColors,
+                singleLine = true,
+                isError = isError,
+                trailingIcon = {
+                    if (isError)
+                        Icon(painterResource(id = R.drawable.error),"error", tint = MaterialTheme.colorScheme.error)
+                }
+                )
+
+        }
     }
 }
 
@@ -359,7 +499,7 @@ fun BottomNavigationBar(
     ) { innerPadding ->
         NavHost(
             navController,
-            startDestination = Screen.Overview.route,
+            startDestination = Screen.AddValue.route,
             Modifier.padding(innerPadding)
         ) {
             composable(Screen.Overview.route) { OverviewScreen() }
